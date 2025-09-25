e76144b2374f0ab49a2ff53af94668f65eef9bfc
camile vitoria <camile.vitoria345@gmail.com>
2025-09-25 10:36:21 -0300

    canvas

---
+++ canvas/script.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+context.fillStyle = "red";
+context.fillRect(0, 0, 200, 200);
+
+context.fillStyle = "green";
+context.fillRect(200, 5, 290,190);
+
+context.fillStyle = "yellow";
+context.fillRect(5, 5, 190, 190);

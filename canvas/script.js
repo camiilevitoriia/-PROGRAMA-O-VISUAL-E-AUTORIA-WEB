7eb93f908a96def0ba2d8dfdd4aeec26f8e99cc3
camile vitoria <camile.vitoria345@gmail.com>
2025-09-25 10:39:28 -0300

    canvas

--- canvas/script.js
+++ canvas/script.js
@@ -3,8 +3,9 @@ let context = canvas.getContext("2d");
 context.fillStyle = "red";
 context.fillRect(0, 0, 200, 200);
 
-context.fillStyle = "green";
-context.fillRect(200, 5, 290,190);
-
 context.fillStyle = "yellow";
-context.fillRect(5, 5, 190, 190);
+context.fillRect(200, 5, 290, 190);
+
+context.fillStyle = "green";
+context.fillRect(5, 5, 190,190);
+

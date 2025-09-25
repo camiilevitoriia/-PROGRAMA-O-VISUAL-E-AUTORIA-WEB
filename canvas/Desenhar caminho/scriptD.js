669340b8dcdaf9ed0066759d32d4851731437aa0
camile vitoria <camile.vitoria345@gmail.com>
2025-09-25 10:54:23 -0300

    Desenhos

---
+++ canvas/Desenhar caminho/scriptD.js
@@ -0,0 +1,23 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "rgba(200, 0, 0, 0.5)";
+ctx.moveT(0,0);
+ctx.lineTo(200, 100);
+ctx.closePath();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "rgb(0, 0, 200)";
+ctx.moveTo(250, 250);
+ctx.lineTo(200, 200);
+ctx.closePath();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.moveTo(200, 200);
+ctx.lineTo(0, 200);
+ctx.closePath();
+ctx.stroke();
